978a786e4bff902dcb0af28e747d9c691a33e10e
unknown <phamvietcuong12@gmail.com>
2021-11-02 14:52:42 +0700

    Truy van du lieu csdl

--- src/QuanLySinhVien.sql
+++ src/QuanLySinhVien.sql
@@ -75,4 +75,23 @@ from Student S join mark m on S.StudentId = M.StudentId join Subject Sub on M.Su
 select S.StudentId, S.StudentName, Sub.Subname, M.Mark
 from Student S join mark m on S.StudentId = M.StudentId join Subject Sub on m.SubId = Sub.SubId
 where SubName = 'CF';
+# Hiển thị tất cả các sinh viên có tên bắt đầu bảng ký tự ‘h’
+select StudentName from Student where StudentName like 'h%'
 
+#     Hiển thị các thông tin lớp học có thời gian bắt đầu vào tháng 12.
+select StartDate from Class where MONTH(StartDate) = 12;
+
+# Hiển thị tất cả các thông tin môn học có credit trong khoảng từ 3-5.
+select * from Subject where Credit between 3 and 5;
+
+
+# Thay đổi mã lớp(ClassID) của sinh viên có tên ‘Hung’ là 2.
+update student set ClassId=2 where StudentName = 'Hung';
+
+# Hiển thị các thông tin: StudentName, SubName, Mark. Dữ liệu sắp xếp theo điểm thi (mark) giảm dần.
+# nếu trùng sắp theo tên tăng dần.
+select S.StudentName, Sub.Subname, M.Mark
+from((mark m
+    join student s on m.StudentId = s.StudentId)
+    join Subject sub on  m.SubId = Sub.SubId)
+order by Mark asc;
